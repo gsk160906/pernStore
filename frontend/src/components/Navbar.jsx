3b001e2c86b6a2b8184d77717d4035f7bb4b87af
gsk160906 <gowthamganesansk.cse2024@citchennai.net>
2026-05-29 11:25:20 +0530

    dynamic cart size

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,14 @@ import { Link, useResolvedPath } from "react-router-dom";
 import {ShoppingCartIcon,ShoppingBagIcon} from "lucide-react"
 import ThemeSelector from "./ThemeSelector";
 import { useThemeStore } from "../store/useThemeStore";
+import { useProductStore } from "../store/useProductStore";
 
 
 function Navbar() {
     const { pathname } = useResolvedPath();
     const isHomePage = pathname === "/";
 
-    
+    const {products} = useProductStore();
 
   return (
     <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50'>
@@ -38,7 +39,7 @@ function Navbar() {
                         <div className="indicator">
                             <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
                                 <ShoppingBagIcon className="size-5" />
-                                    <span className="badge badge-sm badge-primary indicator-item">9</span>
+                                    <span className="badge badge-sm badge-primary indicator-item">{products.length}</span>
                             </div>
                         </div>    
                     )}
